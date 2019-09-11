56589151f001c65f756ee7e4586fffc73319487e
Nataliia Globa <Nataliia_Globa@epam.com>
2019-09-11 22:38:01 +0200

    NewMailCreation and SendingMailFromDraft tests were added

--- src/test/java/com/epam/mypo/tests/DeleteTest.java
+++ src/test/java/com/epam/mypo/tests/DeleteTest.java
@@ -2,17 +2,17 @@ package com.epam.mypo.tests;
 
 import com.epam.mypo.HomePage;
 import com.epam.mypo.InboxPage;
-import com.sun.xml.internal.messaging.saaj.util.Base64;
 import org.testng.annotations.Test;
 
 public class DeleteTest extends BaseTest {
 
     @Test
-    public void deleteTest() {
+    public InboxPage deleteTest() {
         HomePage homePage = new HomePage(driver);
         homePage.open();
         homePage.fillLogin("nataliia.globa", "TestTest753");
         InboxPage inboxPage = homePage.pressLogin();
+
 
 
     }
